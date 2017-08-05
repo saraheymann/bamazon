7fc2aa7960f1989ae80f8486b350160dc828c8fd
sara heymann <saraheymann@saras-mbp.attlocal.net>
2017-08-04 23:23:41 -0500

    connected inquirer to items in stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,9 +19,12 @@ function selectData() {
         if (err) {
             throw err;
         }
-        data.forEach(function (row) {
-            console.log(`ID is: ${row.item_id}, The product is: ${row.product_name}, Price: $${row.price}`);
-        })
+        // data.forEach(function (row) {
+        //     console.log(`ID is: ${row.item_id}, The product is: ${row.product_name}, Price: $${row.price}`);
+        // })
+         for(var i = 0; i < data.length; i++) {
+          	console.log("Item ID: " + data[i].item_id + " | Product: " + data[i].product_name + " |  Price: " +  data[i].price + " | Quantity: " + data[i].stock_quantity);
+}
         inquirer.prompt([
           {
             'name': 'ID',
@@ -32,21 +35,26 @@ function selectData() {
             'message': 'How many units would you like to buy?'
         }
     ]).then(function(answer){
-          var chosenItem;
-          for(var i = 0; i< data.length; i++){
-              if(data[i].item.id === answer.ID){
-                  chosenItem = data[i];
-              }
-          }
-            if(chosenItem.stock_quantity > answer.units){
-                // var newStock = chosenItem.stock_quantity - answer.units;
-                connection.query(
-                function(error){
-                    if (error) throw err;
-                    console.log("purchased successfully");
-                }
-                )
+        //   var chosenItem;
+        //   for(var i = 0; i< data.length; i++){
+        //       if(data[i].item.id === answer.ID){
+        //           chosenItem = data[i];
+        //       }
+        //   }
+        //     if(chosenItem.stock_quantity > answer.units){
+        //         // var newStock = chosenItem.stock_quantity - answer.units;
+        //             console.log("purchased successfully");
+                
+                
+        //     }
+        connection.query('SELECT * FROM  products WHERE item_id=' + answer.ID, function(err,data){
+            if (err) throw err;
+            if(data[0].stock_quantity - answer.units >= 0){
+                console.log("item is in stock");
+            }else{
+                console.log("your item is out of stock");
             }
+        })
         
     })
         
